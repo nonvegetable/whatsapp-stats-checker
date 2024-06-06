85b50bac03b5f5b990e0431eff06414b790f4ea7
Vighnesh Palande <100308933+nonvegetable@users.noreply.github.com>
2024-06-06 11:18:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import re
 
 # open the export chat file and put the file path on the first argument
-filePath = open("Your/File/Path", "r")
+with open("your-file-path", "r", encoding="utf-8") as filePath:
 
-# a dictionary to store the usernames
-# amount of messages which will update dynamically
-userDictionary = {}
+    # a dictionary to store the usernames
+    # amount of messages which will update dynamically
+    userDictionary = {}
 
-# create a while loop to check through the file
-while False:
-    # look for the character "]"
-    # incrimentally add the letters to the key of the dictionary
-    # stop doing this when ":" is found
-    # add letters to the key of the dictionary
-        m = re.search('](.+?):', text)
+    # read the file line by line
+    for line in filePath:
+        # look for the character "]"
+        # incrementally add the letters to the key of the dictionary
+        # stop doing this when ":" is found
+        # add letters to the key of the dictionary
+        m = re.search('](.+?):', line)
         if m:
             found = m.group(1)
+            # update the userDictionary
+            if found in userDictionary:
+                userDictionary[found] += 1
+            else:
+                userDictionary[found] = 1
+
+# print the dictionary with values 
+print(userDictionary)
