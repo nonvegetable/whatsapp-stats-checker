3a7817de8f9f1c11face639aa95c44cea2da5c20
nonvegetable <100308933+nonvegetable@users.noreply.github.com>
2024-09-09 15:07:26 +0530

    iphone and android

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import re
 
-with open("_chat.txt", "r", encoding="utf-8") as filePath:
+with open("WhatsApp Chat with We❤️.txt", "r", encoding="utf-8") as filePath:
     userDictionary = {}
-    for line in filePath:
-        m = re.search('](.+?):', line)
-        if m:
-            found = m.group(1)
-            if found in userDictionary:
-                userDictionary[found] += 1
-            else:
-                userDictionary[found] = 1
-print(userDictionary)
+    device = input("Press Press A for Android and I for iPhone: ")
+    if device == "a":
+        for line in filePath:
+            m = re.search('-(.+?):', line)
+            if m:
+                found = m.group(1)
+                if found in userDictionary:
+                    userDictionary[found] += 1
+                else:
+                    userDictionary[found] = 1
+    elif device == "i":
+        for line in filePath:
+            m = re.search('] (.+?):', line)
+            if m:
+                found = m.group(1)
+                if found in userDictionary:
+                    userDictionary[found] += 1
+                else:
+                    userDictionary[found] = 1
+    print(userDictionary)
